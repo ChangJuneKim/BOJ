4c803c8e8d86b6a0825ddb7972c1988115d364b2
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2022-08-24 00:18:45 +0900

    [Gold IV] Title: 집합의 표현, Time: 484 ms, Memory: 50292 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.java
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.java
@@ -4,72 +4,86 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	int[] parents;
-	
+
+	public class UnionFind {
+		int[] parent;
+		int count;
+
+		public UnionFind(int count) {
+			this.count = count;
+			parent = new int[this.count];
+		}
+
+		public void makeSet() {
+			for (int i = 0; i < count; i++) {
+				parent[i] = -1;
+			}
+		}
+
+		private int find(int num) {
+			if (parent[num] < 0) {
+				return num;
+			}
+
+			return parent[num] = find(parent[num]);
+		}
+
+		private void merge(int num1, int num2) {
+			int rootA = find(num1);
+			int rootB = find(num2);
+
+			if (rootA == rootB) {
+				return;
+			}
+
+			int temp = parent[rootA] + parent[rootB];
+
+			if (parent[rootA] > parent[rootB]) {
+				parent[rootA] = rootB;
+				parent[rootB] = temp;
+			} else {
+				parent[rootB] = rootA;
+				parent[rootA] = temp;
+			}
+		}
+	}
+
 	public void solution() throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-		
+
 		int N = Integer.valueOf(st.nextToken());
 		int M = Integer.valueOf(st.nextToken());
-		
-		parents = new int[N + 1];
-		
-		for (int i = 0; i <= N; i++) {
-			parents[i] = -1;
-		}
-		
+
+		UnionFind uf = new UnionFind(N + 1);
+		uf.makeSet();
+
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			
+
 			int command = Integer.valueOf(st.nextToken());
 			int a = Integer.valueOf(st.nextToken());
 			int b = Integer.valueOf(st.nextToken());
-			
-			if(command == 0) {
-				merge(a, b);
-			} 
 
-			if(command == 1){
-				int rootA = find(a);
-				int rootB = find(b);
-				
+			if (command == 0) {
+				uf.merge(a, b);
+			}
+
+			if (command == 1) {
+				int rootA = uf.find(a);
+				int rootB = uf.find(b);
+
 				if (rootA == rootB) {
-					sb.append("YES").append("\n");
+					sb.append("YES")
+						.append("\n");
 				} else {
-					sb.append("NO").append("\n");
+					sb.append("NO")
+						.append("\n");
 				}
-			}	
+			}
 		}
-		System.out.println(sb);	
-	}
-
-	private int find(int num) {
-		if (parents[num] < 0) {
-			return num;
-		}
-
-		return parents[num] = find(parents[num]);
-	}
-
-	private void merge(int num1, int num2) {
-		int rootA = find(num1);
-		int rootB = find(num2);
-
-		if (rootA == rootB) {
-			return;
-		}
-
-		int temp = parents[rootA] + parents[rootB];
-
-		if (parents[rootA] > parents[rootB]) {
-			parents[rootA] = rootB;
-			parents[rootB] = temp;
-		} else {
-			parents[rootB] = rootA;
-			parents[rootA] = temp;
-		}
+		System.out.println(sb);
 	}
 
 	public static void main(String[] args) throws IOException {
