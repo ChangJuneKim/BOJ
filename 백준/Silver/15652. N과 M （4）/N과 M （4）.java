585936bd3a1c1ac45e66872ca3ea5f8070ecc25a
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2022-10-10 18:51:10 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 20056 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    int N, M;
+    
+    ArrayList<Integer> result;
+    boolean[] used;
+    
+    StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+    
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        
+        result = new ArrayList<>();
+        used = new boolean[N + 1];
+        dfs(0, 0);
+        System.out.println(sb);
+    }
+    
+    private void dfs(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < result.size(); i++) {
+                sb.append(result.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = start; i < N; i++) {
+            result.add(i + 1);
+            dfs(i, depth + 1);
+            result.remove(result.size() - 1);
+        }
+        
+    }
+}
