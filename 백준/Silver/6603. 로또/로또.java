9d32a4a40cd88091dcef078b649d6685c99a226e
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2022-10-10 22:20:49 +0900

    [Silver II] Title: 로또, Time: 144 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,57 +1,62 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
-	int[] numbers;
-	int[] result;
-
-	StringBuilder sb = new StringBuilder();
-
-	private void combination(int depth, int start) {
-		if (depth == 6) {
-			for (int i = 0; i < result.length; i++) {
-				sb.append((result[i] + " "));
-			}
-			sb.append("\n");
-			return;
-		}
-
-		for (int i = start; i < numbers.length; i++) {
-			result[depth] = numbers[i];
-			combination(depth + 1, i + 1);
-		}
-	}
-
-	public void solution() throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-
-		while (true) {
-			String input = br.readLine();
-			if (input.equals("0")) {
-				break;
-			}
-
-			st = new StringTokenizer(input);
-			int k = Integer.valueOf(st.nextToken());
-
-			numbers = new int[k];
-			result = new int[6];
-
-			for (int i = 0; i < k; i++) {
-				numbers[i] = Integer.valueOf(st.nextToken());
-			}
-
-			combination(0, 0);
-			sb.append("\n");
-		}
-		System.out.println(sb);
-
-	}
-
-	public static void main(String[] args) throws Exception {
-		new Main().solution();
-	}
+    
+    int k;
+    int[] numbers;
+    ArrayList<Integer> result;
+    
+    StringBuilder sb = new StringBuilder();
+    
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+    
+    public void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("0")) {
+                break;
+            }
+            
+            st = new StringTokenizer(input);
+            
+            k = Integer.parseInt(st.nextToken());
+            numbers = new int[k];
+            for (int i = 0; i < k; i++) {
+                numbers[i] = Integer.parseInt(st.nextToken());
+            }
+            
+            Arrays.sort(numbers);
+            result = new ArrayList<>();
+            lotto(0, 0);
+            sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+    
+    private void lotto(int start, int depth) {
+        if (depth == 6) {
+            for (int i = 0; i < result.size(); i++) {
+                sb.append(result.get(i)).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        
+        for (int i = start; i < k; i++) {
+            result.add(numbers[i]);
+            lotto(i + 1, depth + 1);
+            result.remove(result.size() - 1);
+        }
+    }
+    
 }
