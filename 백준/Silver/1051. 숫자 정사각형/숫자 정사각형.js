86ef7981008faa9bb0beb7a8f181212f65c0b530
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2023-09-05 00:00:08 +0900

    [Silver III] Title: 숫자 정사각형, Time: 188 ms, Memory: 10604 KB -BaekjoonHub

---
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+const [N, M] = input
+    .shift()
+    .split(" ")
+    .map((item) => +item);
+
+let result = 1;
+let start = 0;
+let width = 0;
+
+for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+        start = +input[i][j]; 
+        for (let k = j + 1; k < M; k++) {
+            if (+input[i][k] === start) { 
+                width = k - j + 1; 
+                if (
+                    i + width - 1 < N && 
+                    start === +input[i + width - 1][j] && 
+                    input[i + width - 1][j] === input[i + width - 1][k] && 
+                    width * width > result 
+                ) {
+                    result = width * width; 
+                }
+            }
+        }
+    }
+}
+
+console.log(result);
