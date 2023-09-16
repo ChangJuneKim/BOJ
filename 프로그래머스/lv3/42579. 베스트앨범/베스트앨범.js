ab969d70db848667a9b84189579f30725a2d7402
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2023-09-16 20:12:31 +0900

    [level 3] Title: 베스트앨범, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.js
@@ -0,0 +1,26 @@
+function solution(genres, plays) {
+    const map = genres.reduce((acc, genre, idx) => {
+        if (!acc[genre]) {
+            acc[genre] = {
+                totalPlay: 0,
+                songs: []
+            };
+        }
+        acc[genre].totalPlay += plays[idx];
+        acc[genre].songs.push({ idx, play: plays[idx] });
+        return acc;
+    }, {});
+
+    // 각 장르별로 노래를 재생 횟수와 고유 번호로 정렬
+    for (let genre in map) {
+        map[genre].songs.sort((a, b) => b.play - a.play || a.idx - b.idx);
+    }
+
+    // 총 재생 횟수가 높은 장르부터 최대 2개의 노래를 수록
+    return [].concat(
+        ...Object.values(map)
+            .sort((a, b) => b.totalPlay - a.totalPlay)
+            .map((genre) => genre.songs.slice(0, 2))
+            .map((songs) => songs.map((song) => song.idx))
+    );
+}
