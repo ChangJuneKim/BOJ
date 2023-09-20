6c084525e32ba13c22ad36425991819d027a7b11
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2023-09-20 22:27:55 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -1,25 +1,9 @@
-function solution(n, input) {
-    let th;
-    let count = 1;
+function solution(n, words) {
+    let answer = 0;
+    words.reduce((prev, now, idx) => {
+        answer = answer || ((words.slice(0, idx).indexOf(now) !== -1 || prev !== now[0]) ? idx : answer);
+        return now[now.length-1];
+    }, "")
 
-    const prev = Array.from({length: n + 1}, () => []);
-    console.log(prev)
-    const words = [...input];
-    
-    for(let i = 0; i < words.length; i++){
-        th = (i % n) + 1;
-        count++;
-        
-        if(prev.flat().includes(words[i])){
-            return [th, prev[th].length + 1]
-        }
-        
-        if(i > 0 && words[i].at(0) !== words[i - 1]?.at(-1)){
-            return [th, prev[th].length + 1]
-        } 
-        
-        prev[th].push(words[i]);
-    }
-    
-    return [0, 0]
+    return answer ? [answer%n+1, Math.floor(answer/n)+1] : [0,0];
 }
