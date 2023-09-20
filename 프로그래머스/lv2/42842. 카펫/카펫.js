56be56f8159fe4fa5bf24d93d0d26d5eca81ee80
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2023-09-20 16:29:02 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -1,12 +1,28 @@
 function solution(brown, yellow) {
-    let total = brown + yellow;
+    let factors = getFactors(yellow);
 
-    for (let height = 3; height <= total; height++) {
-        if (total % height === 0) {
-            let width = total / height;
-            if ((width - 2) * (height - 2) === yellow) {
-                return [width, height];
-            }
+    for (let i = 0; i < factors.length; i++) {
+        let height = factors[i]; // 높이와 너비의 위치 변경
+        let width = yellow / height;
+        
+        // 갈색 격자의 수를 계산
+        let currentBrown = 2 * width + 2 * height + 4;
+        
+        if (currentBrown === brown) {
+            return [width + 2, height + 2]; // 노란색 격자 주위에 갈색 격자가 있으므로 +2를 합니다.
         }
     }
 }
+
+function getFactors(num) {
+    let factors = [];
+    
+    for (let i = 1; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            factors.push(i);
+        }
+    }
+
+    return factors;
+}
+
