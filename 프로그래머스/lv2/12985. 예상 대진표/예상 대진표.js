88a79fbc6d9184bf81be85b770b5940fc0962182
김창준 <77381154+ChangJuneKim@users.noreply.github.com>
2023-09-21 00:54:39 +0900

    [level 2] Title: 예상 대진표, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,14 @@
+function solution(N, A, B) {
+    let round = 0;
+    
+    while (A !== B) {
+        A = Math.ceil(A / 2);
+        B = Math.ceil(B / 2);
+        round++;
+    }
+    
+    return round;
+}
+
+// 예시
+console.log(solution(8, 4, 7)); // 출력: 3
