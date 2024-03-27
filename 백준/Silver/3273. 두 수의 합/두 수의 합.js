5cf289f17d4a7f1c034bd24b96446c94a72dfd0b
김창준 <kchang6869@gmail.com>
2024-03-27 18:12:15 +0900

    [Silver III] Title: 두 수의 합, Time: 212 ms, Memory: 18660 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.js
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.js
@@ -1,34 +1,27 @@
-const fs = require('fs');
-const filePath = '/dev/stdin'
+const input = require('fs').readFileSync(process.version === 'v20.9.0' ? 'input.txt' : '/dev/stdin').toString().trim().split('\n');
 
-const input = fs.readFileSync(filePath).toString().trim().split('\n');
-
-const solution = input => {
-  const n = +input[0]; // 수열의 크기
-  const arr = input[1].split(' ').map(v => +v); // 주어진 수열
-  const x = +input[2]; // 찾을 값(arr[p1] + arr[p2])
-
-  let p1 = 0;
-  let p2 = n - 1;
+const solution = () => {
+  const N = +input[0];
+  const numbers = input[1].split(' ').map(Number).sort((a, b) => a - b);
+  const X = +input[2];
 
   let count = 0;
 
-  const sortedArr = arr.sort((a, b) => a - b);
-  let sum = 0;
+  let left = 0;
+  let right = N - 1;
 
-  while (p1 < p2) {
-    sum = sortedArr[p1] + sortedArr[p2];
-    if (sum === x) {
+  while(left < right){
+    const sum = numbers[left] + numbers[right];
+
+    if(sum > X) right--;
+    else if(sum < X) left++;
+    else {
+      left++;
       count++;
-    }
-    if (sum <= x) {
-      p1++;
-    } else {
-      p2--;
     }
   }
 
   console.log(count);
 };
 
-solution(input);
+solution();
